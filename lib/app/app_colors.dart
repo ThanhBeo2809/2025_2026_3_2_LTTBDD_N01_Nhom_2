b52097dc692c422f083b009158d08ed3481fce73
Phạm Trung Thành <24100326@st.phenikaa-uni.edu.vn>
2026-08-05 08:57:56 +0700

    Thêm bảng màu cho ứng dụng

---
+++ lib/app/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const Color primary = Color(0xFFE85D3F);
+  static const Color secondary = Color(0xFFF2A93B);
+  static const Color accent = Color(0xFF2F8F6B);
+  static const Color cream = Color(0xFFFFF8F1);
+  static const Color ink = Color(0xFF292321);
+}
