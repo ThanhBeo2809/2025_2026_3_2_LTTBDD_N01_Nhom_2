c4f0bbca5d82f9a407c71b005961c81f62389b4a
Phạm Trung Thành <24100326@st.phenikaa-uni.edu.vn>
2026-08-05 09:06:04 +0700

    Tổng hợp danh sách công thức mẫu

---
+++ lib/data/mock_recipes.dart
@@ -0,0 +1,16 @@
+import '../models/recipe.dart';
+import 'recipes/banh_flan_recipe.dart';
+import 'recipes/banh_mi_trung_recipe.dart';
+import 'recipes/com_chien_rau_cu_recipe.dart';
+import 'recipes/goi_cuon_recipe.dart';
+import 'recipes/pho_bo_recipe.dart';
+import 'recipes/salad_uc_ga_recipe.dart';
+
+const mockRecipes = <Recipe>[
+  phoBoRecipe,
+  goiCuonRecipe,
+  banhMiTrungRecipe,
+  comChienRauCuRecipe,
+  banhFlanRecipe,
+  saladUcGaRecipe,
+];
